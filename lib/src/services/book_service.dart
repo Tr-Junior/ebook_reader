5faddd775aee8da26d8b3c0989a2a46a97270d62
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 10:57:20 -0300

    feat: add book service

---
+++ lib/src/services/book_service.dart
@@ -0,0 +1,15 @@
+import 'package:ebook_reader/src/models/book.dart';
+
+class BookService {
+  Future<List<Book>> getBooks() async {
+    final response =
+        await http.get(Uri.parse('https://escribo.com/books.json'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((json) => Book.fromJson(json)).toList();
+    } else {
+      throw Exception('Erro ao carregar os livros');
+    }
+  }
+}
