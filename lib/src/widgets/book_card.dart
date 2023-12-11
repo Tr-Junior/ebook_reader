b9ea3c19fe2b26af681462594f852676d1cdd53b
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 16:19:57 -0300

    chore: remove references of reader_screen

--- lib/src/widgets/book_card.dart
+++ lib/src/widgets/book_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ebook_reader/src/models/book.dart';
-import 'package:ebook_reader/src/screens/reader_screen.dart';
 
 class BookCard extends StatelessWidget {
   final Book book;
@@ -18,40 +17,29 @@ class BookCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ReaderScreen(book: book),
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(book.coverUrl, fit: BoxFit.cover),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(book.title),
           ),
-        );
-      },
-      child: Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.network(book.coverUrl, fit: BoxFit.cover),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(book.title),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  icon:
-                      Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
-                  onPressed: onFavoritePressed,
-                ),
-                IconButton(
-                  icon: Icon(Icons.account_box),
-                  onPressed: onReadPressed,
-                ),
-              ],
-            ),
-          ],
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+                onPressed: onFavoritePressed,
+              ),
+              IconButton(
+                icon: Icon(Icons.read_more),
+                onPressed: onReadPressed,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
