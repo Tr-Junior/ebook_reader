743ea5d10dae4f0b1359bd55f620085c65661635
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 11:23:00 -0300

    refactor: update book_details_scren.dart add missing import

--- lib/src/screens/book_details_scren.dart
+++ lib/src/screens/book_details_scren.dart
@@ -1,4 +1,5 @@
 import 'package:ebook_reader/src/models/book.dart';
+import 'package:ebook_reader/src/screens/reader_screen.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsScreen extends StatelessWidget {
