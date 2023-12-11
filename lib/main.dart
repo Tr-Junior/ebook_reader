775d709947e0d63525aaf2137d371968bc40a9a7
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 13:51:28 -0300

    refactor: update main.dart remove const

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:ebook_reader/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
