98a6d724ab099b40425e3e767487260dd4dc4aca
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 11:00:15 -0300

    refactor: add json reader to book model

--- lib/src/models/book.dart
+++ lib/src/models/book.dart
@@ -3,4 +3,11 @@ class Book {
   final String coverUrl;
 
   Book({required this.title, required this.coverUrl});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      title: json['title'],
+      coverUrl: json['cover_url'],
+    );
+  }
 }
