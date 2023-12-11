731cc48e42a487b27e9512a86b6e83f8981bde7d
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-11 13:52:13 -0300

    refactor: add atributes for reader json book.dart

--- lib/src/models/book.dart
+++ lib/src/models/book.dart
@@ -1,13 +1,26 @@
+// book.dart
 class Book {
+  final int id;
   final String title;
+  final String author;
   final String coverUrl;
+  final String downloadUrl;
 
-  Book({required this.title, required this.coverUrl});
+  Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.coverUrl,
+    required this.downloadUrl,
+  });
 
   factory Book.fromJson(Map<String, dynamic> json) {
     return Book(
+      id: json['id'],
       title: json['title'],
+      author: json['author'],
       coverUrl: json['cover_url'],
+      downloadUrl: json['download_url'],
     );
   }
 }
