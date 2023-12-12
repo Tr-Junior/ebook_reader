fcf90db8cea4d8c01ea002adeac701d3b5297e57
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-12 01:22:38 -0300

    feat: add logic to landscape list of books

--- lib/src/screens/favorite_book_screen.dart
+++ lib/src/screens/favorite_book_screen.dart
@@ -25,10 +25,10 @@ class _FavoriteBooksScreenState extends State<FavoriteBooksScreen> {
   @override
   void initState() {
     super.initState();
-    initSharedPreferences();
+    _initSharedPreferences();
   }
 
-  Future<void> initSharedPreferences() async {
+  Future<void> _initSharedPreferences() async {
     prefs = await SharedPreferences.getInstance();
   }
 
@@ -38,37 +38,51 @@ class _FavoriteBooksScreenState extends State<FavoriteBooksScreen> {
       appBar: AppBar(
         title: const Text('Livros Favoritos'),
       ),
-      body: ListView.builder(
-        itemCount: widget.books.length,
-        itemBuilder: (context, index) {
-          final book = widget.books[index];
-          return BookCard(
-            book: book,
-            isFavorite: true,
-            onFavoritePressed: () {
-              widget.onFavoriteToggled(book);
-              // Remover o livro dos favoritos e atualizar a lista
-              setState(() {
-                widget.books.remove(book);
-              });
-              updateFavorites();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Livro removido dos favoritos: ${book.title}'),
-                  duration: const Duration(seconds: 2),
-                ),
-              );
-            },
-            onReadPressed: (Book selectedBook) {
-              downloadAndOpenBook(context, selectedBook);
-            },
-          );
-        },
-      ),
+      body: _buildBookList(),
     );
   }
 
-  Future<void> downloadAndOpenBook(BuildContext context, Book book) async {
+  Widget _buildBookList() {
+    return widget.books.isNotEmpty
+        ? GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 180,
+              crossAxisSpacing: 6.0,
+              mainAxisSpacing: 6.0,
+              childAspectRatio: 0.6,
+            ),
+            itemCount: widget.books.length,
+            itemBuilder: (context, index) {
+              final book = widget.books[index];
+              return BookCard(
+                book: book,
+                isFavorite: true,
+                onFavoritePressed: () {
+                  widget.onFavoriteToggled(book);
+                  setState(() {
+                    widget.books.remove(book);
+                  });
+                  _updateFavorites();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content:
+                          Text('Livro removido dos favoritos: ${book.title}'),
+                      duration: const Duration(seconds: 2),
+                    ),
+                  );
+                },
+                onReadPressed: (Book selectedBook) {
+                  _downloadAndOpenBook(context, selectedBook);
+                },
+              );
+            },
+          )
+        : const Center(
+            child: Text('Nenhum livro favorito.'),
+          );
+  }
+
+  Future<void> _downloadAndOpenBook(BuildContext context, Book book) async {
     try {
       final String downloadedBookPath =
           await BookService().downloadOrOpenBook(book);
@@ -88,7 +102,7 @@ class _FavoriteBooksScreenState extends State<FavoriteBooksScreen> {
     }
   }
 
-  Future<void> updateFavorites() async {
+  Future<void> _updateFavorites() async {
     final List<int> favoriteBookIds =
         widget.books.map((book) => book.id).toList();
     await prefs.setStringList(
