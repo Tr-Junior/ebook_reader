218908dbc3ceb6426d486e9321cb1e52a29ee0fb
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-13 14:32:45 -0300

    refactor: move logic favorite books to book_utils

--- lib/src/utils/book_utils.dart
+++ lib/src/utils/book_utils.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:read_up/src/utils/error_handling.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vocsy_epub_viewer/epub_viewer.dart';
 
 import '../models/book.dart';
 import '../services/book_service.dart';
+
+var logger = Logger();
 
 class BookUtils {
   static Future<void> downloadAndOpenBook(
@@ -23,6 +29,26 @@ class BookUtils {
       VocsyEpub.open(downloadedBookPath);
     } catch (error) {
       print('Erro ao abrir o livro: $error');
+    }
+  }
+}
+
+class FavoriteBookManager {
+  static Future<void> toggleFavorite({
+    required Book book,
+    required List<int> favoriteBookIds,
+    required SharedPreferences prefs,
+    required Function(List<int>) onFavoritesUpdated,
+  }) async {
+    try {
+      var tempOutput = List<int>.from(favoriteBookIds);
+      tempOutput.contains(book.id)
+          ? tempOutput.remove(book.id)
+          : tempOutput.add(book.id);
+      prefs.setString('books_id', utf8.decode(tempOutput));
+      onFavoritesUpdated(tempOutput);
+    } on AppException catch (e) {
+      logger.e('Erro ao alterar favorito: $e');
     }
   }
 }
