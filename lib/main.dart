0399ef4a569aa829b02edd327e6140ecaf86ac7f
Tr-Junior <tarcisio.junior1406@gmail.com>
2023-12-13 00:16:38 -0300

    refactor: update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
-import 'package:ebook_reader/src/screens/home_screen.dart';
+import 'package:ReadUP/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ebook Reader',
+      title: 'ReadUp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
